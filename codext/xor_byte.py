1fa82c1fd121cf224fd02462571a08edc4312da2
dhondta <alexandre.dhondt@gmail.com>
2020-02-02 16:54:20 +0100

    Minor adaptations

--- codext/xor_byte.py
+++ codext/xor_byte.py
@@ -19,10 +19,9 @@ def xor_byte_encode(i):
         r = _xorn(ensure_str(text), i)
         return r, len(r)
     return encode
-xor_byte_decode = xor_byte_encode
 
 
 # note: the integer behind "xor" is captured for sending to the parametrizable
 #        encode and decode functions "xor_byte_**code"
-add("xorN", xor_byte_encode, xor_byte_decode,
+add("xorN", xor_byte_encode, xor_byte_encode,
     r"(?i)xor[-_]?([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$")
