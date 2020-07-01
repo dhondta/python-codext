1f0c00df2a241da8b4c7a192926ca9cf4bf7a725
dhondta <alexandre.dhondt@gmail.com>
2020-07-01 07:51:31 +0200

    Slightly modified markdown codec

--- codext/others/markdown.py
+++ codext/others/markdown.py
@@ -6,13 +6,13 @@ This codec:
 - encodes strings from bytes to bytes
 - encodes file content from str to bytes (write)
 """
-from markdown2 import markdown
+from markdown2 import markdown as md2html
 
 from ..__common__ import *
 
 
 def markdown_encode(mdtext, errors="strict"):
-    return markdown(mdtext), len(mdtext)
+    return md2html(mdtext), len(mdtext)
 
 
 # note: the group is NOT captured so that the pattern is only used to match the name of the codec and not to dynamically
