ed14c33d5c191919f6e302f9b4c5f5a11baa9c6e
dhondta <alexandre.dhondt@gmail.com>
2022-01-19 15:16:12 +0100

    Fixed codec: rick

--- codext/stegano/rick.py
+++ codext/stegano/rick.py
@@ -20,8 +20,9 @@ ENCMAP = {
     'A': "NEVER", 'B': "GONNA", 'C': "GIVE", 'D': "YOU", 'E': "UP", 'F': "Never", 'G': "Gonna", 'H': "LET", 'I': "You",
     'J': "DOWN", 'K': "NEver", 'L': "GOnna", 'M': "TURN", 'N': "AROUND", 'O': "AND", 'P': ["DESERT", "DESSERT"],
     'Q': "YOu", 'R': "NEVer", 'S': "gonna", 'T': "TELL", 'U': "A", 'V': "LIE", 'W': "and", 'X': "HURT", 'Y': "you",
-    'Z': "rick astley", ' ': "+", '.': ".", '\n': "\n",
+    'Z': "rick", ' ': "+", '.': ".", '\n': "\n",
     '0': "0", '1': "1", '2': "2", '3': "3", '4': "4", '5': "5", '6': "6", '7': "7", '8': "8", '9': "9",
+    '': "astley", # silent this token for decoding ("rick astley" causes an issue with the separator " ")
 }
 
 
