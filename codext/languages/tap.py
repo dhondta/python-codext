37cfdbbc09eb32b13bb738ff5009cd7b3325b57b
smarbal <sebastien.martinez99@gmail.com>
2022-01-19 16:41:04 +0100

    Fix error handling

--- codext/languages/tap.py
+++ codext/languages/tap.py
@@ -23,7 +23,6 @@ def build_encmap(map) :
             dict[map[i]] = "" + col * "." + " " + row * "."
             i += 1
     dict['k'] = dict['c']
-    dict[' '] = ''
     return dict
 
 def encode_tap(text, errors = 'strict') : 
@@ -31,20 +30,26 @@ def encode_tap(text, errors = 'strict') :
     ENCMAP = build_encmap(map)
     encoded = ""
     for i, letter in enumerate(text) :
-        encoded += ENCMAP[letter.lower()]
+        try : 
+            encoded += ENCMAP[letter.lower()]
+        except KeyError : 
+            pass
         if i != len(text) - 1 and letter != ' ': 
             encoded += '  '
     return encoded, len(text)    
 
 
-def decode_tap(text, errors = 'strict') : 
+def decode_tap(text, errors = 'ignore') : 
     map = 'abcdefghijlmnopqrstuvwxyz'
     ENCMAP = build_encmap(map)
     decoded = ""
     for elem in text.split("  ") :
-        decoded += next(key for key, value in ENCMAP.items() if value == elem)
+        try : 
+            decoded += next(key for key, value in ENCMAP.items() if value == elem)
+        except StopIteration : 
+            print("Invalid character(s) in the input. This is what could be decoded :")
     return decoded, len(text)    
   
 
-add("tap", encode_tap, decode_tap, ignore_case="encode")
+add("tap", encode_tap, decode_tap, ignore_case="both")
 
